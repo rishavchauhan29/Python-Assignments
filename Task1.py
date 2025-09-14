1b1f94533d580fbbba5472440599ace83089f375
rishavchauhan29 <rishavchauhan252.rc@gmail.com>
2025-09-14 15:00:18 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,12 @@
+#Task : 1 - Perform Basic Mathematical Operations
+
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+a = float(a)
+b = float(b)
+
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ", a/b)
---
+++ Task2.py
@@ -0,0 +1,9 @@
+#Task 2: Create a Personalized Greeting
+
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+
+a = str(a)
+b = str(b)
+
+print("Hello ", a + " " + b, "! Welcome to the Python program.")
